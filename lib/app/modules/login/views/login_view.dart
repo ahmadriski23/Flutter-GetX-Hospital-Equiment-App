9196290277548ce79e871b830f1fd5539e435ee0
ahmadriski23 <ahmadriskixrpl1csr@gmail.com>
2023-10-22 10:57:59 +0700

    update screen

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -4,6 +4,8 @@ import '../components/login_component.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
+  const LoginView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
